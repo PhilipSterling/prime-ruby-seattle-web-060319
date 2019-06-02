7aa1b89eda8e1a6c750af415fdfcaa9db1a90f62
Philip Sterling <philipssterlings@gmail.com>
2019-06-02 04:17:52 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,18 @@
 # Add  code here!
 def prime?(num)
-  (2..num).each { |i|
-  if num % i == 0 && i < num
+  if(num >= 2)
+    (2..num).each { |i|
+    if num % i == 0 && i < num
     return false
-  end
-  }
+    end
+    }
+  elsif(num == 1 || num == 0)
+  return false
+  else
+    (num..2).each { |i|
+    if num % i == 0 && i < num
+    return false
+  end }
+end
   return true
 end
