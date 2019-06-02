68da5b5b1ece4f9ecee85f4afd2a963cbd6e79a2
Philip Sterling <philipssterlings@gmail.com>
2019-06-02 04:10:34 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,5 +2,10 @@
 def prime?(x)
   array = (2..x-1).to_a
   counter = 0
-  for counter in 1..array.length 
+  for counter in 1..array.length do
+    if(x%array[counter] == 0)
+      return true
+    end
+    return false
+      
 end
